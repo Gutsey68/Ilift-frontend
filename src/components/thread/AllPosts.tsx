4b8d10f44ac34352997ff7dfc04a1b3292081784
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-10-20 11:45:31 +0200

    Maj de l'ui des posts

--- src/components/thread/AllPosts.tsx
+++ src/components/thread/AllPosts.tsx
@@ -1,3 +1,4 @@
+import { Earth, Heart, MessageCircle, Send } from 'lucide-react';
 import usersData from '../../../seeds.json';
 import Avatar from '../ui/Avatar';
 
@@ -11,17 +12,35 @@ function AllPosts() {
                             <Avatar alt="" size="sm" src={user.profilePhoto} />
                             <div className="flex flex-col">
                                 <h1 className="font-semibold text-neutral-12">{user.pseudo}</h1>
-                                <p className="text-xs text-neutral-11">@{user.bio}</p>
+                                <div className="flex items-center gap-1 text-xs text-neutral-11">
+                                    <p>{post.createdAt} • </p>
+                                    <Earth size={14} />
+                                </div>
                             </div>
+                        </div>
+                        <div className="flex flex-col px-4">
+                            <h2 className="text-lg font-semibold">{post.title}</h2>
+                            <p className="text-neutral-11">{post.content}</p>
                         </div>
                         <img className="h-auto w-full" src={post.photo} alt={post.title} />
                         <div className="px-4">
-                            <h2 className="text-lg font-semibold">{post.title}</h2>
-                            <p>{post.content}</p>
-                            <div className="flex gap-4 py-4">
-                                <button className="text-neutral-12 hover:text-neutral-11">Like</button>
-                                <button className="text-neutral-12 hover:text-neutral-11">Comment</button>
-                                <button className="text-neutral-12 hover:text-neutral-11">Share</button>
+                            <div className="flex items-center gap-1 border-b border-gray-600 pb-2 text-xs text-neutral-11">
+                                <Heart size={14} />
+                                <p>57</p>
+                            </div>
+                            <div className="flex gap-4 pb-4 pt-2">
+                                <button className="flex items-center gap-2 text-neutral-12 hover:text-green-9">
+                                    <Heart size={16} />
+                                    Like
+                                </button>
+                                <button className="flex items-center gap-2 text-neutral-12 hover:text-green-9">
+                                    <MessageCircle size={16} />
+                                    Comment
+                                </button>
+                                <button className="flex items-center gap-2 text-neutral-12 hover:text-green-9">
+                                    <Send size={16} />
+                                    Share
+                                </button>
                             </div>
                         </div>
                     </div>
