3bae18cbd813355c7b40c7b73c55c3fc9a0b10d9
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-10-24 15:49:12 +0200

    feat: Ajout des erreurs et du onchange sur le formulaire mot de passe oublié

--- src/components/auth/ForgotPasswordForm.tsx
+++ src/components/auth/ForgotPasswordForm.tsx
@@ -1,15 +1,19 @@
+import useValidation from '../../hooks/useValidation';
 import Button from '../ui/Button';
 import { Input } from '../ui/Input';
 
 function ForgotPasswordForm() {
+    const { inputState, error, touched, changeHandler, forgotPwdSubmitHandler } = useValidation();
+
     return (
-        <form className="flex flex-col gap-2">
+        <form onSubmit={forgotPwdSubmitHandler} className="flex flex-col gap-2">
             <h2 className="text-2xl font-semibold">Mot de passe oublié</h2>
             <p className="mb-4 text-sm text-neutral-11">Entrez votre email ci-dessous pour réinitialiser votre mot de passe.</p>
             <label htmlFor="email" className="text-sm">
                 Email
             </label>
-            <Input type="email" name="email" placeholder="Email" required />
+            <Input onChange={changeHandler} value={inputState.email} type="email" name="email" placeholder="Email" />
+            {touched.email && error.email && <p className="mb-2 text-sm text-red-600">Email invalide</p>}
             <Button type="submit" className="mt-2 w-full">
                 Réinitialiser le mot de passe
             </Button>
