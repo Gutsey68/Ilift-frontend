1e20d767364fbee5b192ee26c395e7342fe631cb
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-11-13 20:51:23 +0100

    fix: Suppression de la redirection vers la page de connexion dans PrivateRoute

--- src/components/auth/PrivateRoute.tsx
+++ src/components/auth/PrivateRoute.tsx
@@ -1,10 +1,11 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { useAuthStore } from '../../stores/useAuthStore';
 
 function PrivateRoute() {
   const isAuthenticated = useAuthStore(state => state.isAuthenticated);
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/connexion" />;
+  return isAuthenticated ? <Outlet /> : <Outlet />;
+  //<Navigate to="/connexion" />
 }
 
 export default PrivateRoute;
