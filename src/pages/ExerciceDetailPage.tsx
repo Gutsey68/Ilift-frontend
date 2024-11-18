35419ea1f105c12302c3d623ab671d74cc9d7a49
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-11-18 13:33:54 +0100

    feat: Ajout de la récupération des détails d'un exercice et gestion des résultats

--- src/pages/ExerciceDetailPage.tsx
+++ src/pages/ExerciceDetailPage.tsx
@@ -1,4 +1,37 @@
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import { fetchExerciceAndResults } from '../services/exercicesService';
+
 function ExerciceDetailPage() {
+  const { id } = useParams();
+
+  const {
+    isPending: resultsPending,
+    error: resultsError,
+    data: results
+  } = useQuery({
+    queryKey: ['results', id],
+    queryFn: () => {
+      if (!id) {
+        throw new Error('Utilisateur non connecté');
+      }
+      return fetchExerciceAndResults(id);
+    },
+    enabled: !!id
+  });
+
+  const resultsData = results?.data;
+
+  console.log(resultsData);
+
+  if (resultsPending) {
+    return <div>Chargement...</div>;
+  }
+
+  if (resultsError) {
+    return <div>Erreur: {resultsError.message}</div>;
+  }
+
   return (
     <div className="mx-auto flex min-h-96 w-full max-w-6xl flex-col gap-4">
       <h1 className="text-3xl">Développé décliné avec haltères</h1>
---
+++ src/services/exercicesService.ts
@@ -0,0 +1,26 @@
+export const fetchExerciceAndResults = async (id: string) => {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    throw new Error('Token manquant. Veuillez vous reconnecter.');
+  }
+
+  try {
+    const response = await fetch(`http://localhost:3000/api/exercices/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw { message: errorData.error || 'Non autorisé', status: response.status };
+    }
+
+    return response.json();
+  } catch (error) {
+    throw new Error('Erreur lors de la récupération des données.');
+  }
+};
