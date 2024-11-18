8be39420f6bd01bbc17163ebab6b0ad967e49d9a
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-11-18 20:25:59 +0100

    feat: Mise à jour de la page d'accueil avec un nouveau design et ajout de boutons d'inscription

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,19 @@
+import Button from '../components/ui/Button';
+
 function LandingPage() {
   return (
-    <div>
-      <h1>Landing page</h1>
+    <div className="mx-auto flex w-full max-w-6xl gap-6">
+      <div className="m-auto flex-col items-center text-pretty text-center">
+        {' '}
+        <h1 className="text-5xl font-bold tracking-wider">Trackez vos séances, </h1>
+        <p className="text-5xl font-bold tracking-wider text-green-9">partagez votre progression.</p>
+        <p className="mt-6 text-neutral-10">Rejoignez notre communauté de sportifs,</p>
+        <p className="mb-6 text-neutral-10">et profitez d'une plateforme dédiée pour suivre vos performances et inspirer les autres.</p>
+        <div className="flex justify-center gap-2">
+          <Button className="w-fit">Je m'inscris</Button>
+          <Button className="w-fit border border-neutral-6 bg-neutral-1 text-neutral-11 shadow-sm hover:bg-neutral-2">En savoir plus</Button>
+        </div>
+      </div>
     </div>
   );
 }
