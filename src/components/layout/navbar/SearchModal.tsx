4249784bfc26e5d3c822a1bdab4953b61446c9fd
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-12-24 10:49:30 +0100

    feat: Réorganisation de la barre de recherche avec ajout d'un modal de recherche et suppression de l'ancien hook

---
+++ src/components/layout/navbar/SearchModal.tsx
@@ -0,0 +1,71 @@
+import { useQuery } from '@tanstack/react-query';
+import { X } from 'lucide-react';
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { fetchUsers } from '../../../services/usersService';
+import { UserDetailsType } from '../../../types/userDetailsType';
+import SearchNavSkeletons from '../../skeletons/SearchNavSkeletons';
+import Avatar from '../../ui/Avatar';
+import Card from '../../ui/Card';
+import { Input } from '../../ui/Input';
+import Modal from '../../ui/Modal';
+
+type SearchModalProps = {
+  closeModal: () => void;
+};
+
+function SearchModal({ closeModal }: SearchModalProps) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  const { isPending, data: usersData } = useQuery({
+    queryKey: ['users'],
+    queryFn: fetchUsers
+  });
+
+  const users = usersData.data;
+  const filteredUsers = searchTerm.length >= 2 ? users.filter((user: UserDetailsType) => user.pseudo.toLowerCase().includes(searchTerm.toLowerCase())) : [];
+
+  return (
+    <Modal size="xl" className="top-5" onClose={closeModal}>
+      <Card size="md" className="modal-content max-h-[60vh] overflow-y-auto">
+        <div className="flex items-center gap-4">
+          <Input ref={inputRef} placeholder="Rechercher un utilisateur" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+          <X onClick={closeModal} className="cursor-pointer text-neutral-11 hover:text-neutral-12" />
+        </div>
+        {searchTerm.length >= 2 ? (
+          isPending ? (
+            <SearchNavSkeletons />
+          ) : (
+            <>
+              <hr className="my-4 border-neutral-6" />
+              <div className="flex flex-col gap-2">
+                {filteredUsers.length ? (
+                  filteredUsers.map((user: UserDetailsType) => (
+                    <Link
+                      key={user.id}
+                      to={`/profil/${user.id}`}
+                      className="group flex items-center gap-4 rounded-md p-2 hover:bg-neutral-3"
+                      onClick={closeModal}
+                    >
+                      <Avatar size="sm" src={user.profilePhoto || '/uploads/profil.png'} alt={`Photo de ${user.pseudo}`} />
+                      <p className="text-neutral-11 group-hover:text-green-11">{user.pseudo}</p>
+                    </Link>
+                  ))
+                ) : (
+                  <p className="text-center text-sm text-neutral-11">Aucun utilisateur trouvé</p>
+                )}
+              </div>
+            </>
+          )
+        ) : null}
+      </Card>
+    </Modal>
+  );
+}
+
+export default SearchModal;
--- src/components/layout/navbar/SearchNav.tsx
+++ src/components/layout/navbar/SearchNav.tsx
@@ -1,87 +1,27 @@
-import { useQuery } from '@tanstack/react-query';
-import { Search, X } from 'lucide-react';
+import { Search } from 'lucide-react';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import useSearchBarOnNavigation from '../../../hooks/useSearchBarOnNavigation';
-import { fetchUsers } from '../../../services/usersService';
-import SearchNavSkeletons from '../../skeletons/SearchNavSkeletons';
-import Avatar from '../../ui/Avatar';
-import Card from '../../ui/Card';
 import IconButton from '../../ui/IconButton';
-import { Input } from '../../ui/Input';
-
-interface User {
-  id: string;
-  pseudo: string;
-  profilePhoto?: string;
-}
+import SearchModal from './SearchModal';
 
 export default function SearchNav() {
-  const { handleSearchClick, handleCloseClick, handleBlur, isSearching, inputRef } = useSearchBarOnNavigation();
-  const [searchQuery, setSearchQuery] = useState('');
-  const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const { data: users, isPending } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: fetchUsers
-  });
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const query = event.target.value;
-    setSearchQuery(query);
-
-    if (query.length >= 2 && users) {
-      const filtered = users.filter(user => user.pseudo.toLowerCase().includes(query.toLowerCase()));
-      setFilteredUsers(filtered);
-    } else {
-      setFilteredUsers([]);
-    }
+  const openModal = () => {
+    setIsModalOpen(true);
   };
 
-  const handleLink = () => {
-    handleCloseClick();
-    setSearchQuery('');
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
     <>
-      {!isSearching ? (
-        <div onClick={handleSearchClick} className="relative inline-block">
-          <IconButton icon={<Search className="size-5" />} />
+      <div className="relative inline-block">
+        <div onClick={openModal}>
+          <IconButton icon={<Search className="size-5" />} aria-label="Rechercher des utilisateurs" />
         </div>
-      ) : (
-        <div className="relative inline-block">
-          <div className="flex items-center">
-            <div className="relative">
-              <Input ref={inputRef} type="text" value={searchQuery} placeholder="Rechercher..." onBlur={handleBlur} onChange={handleInputChange} autoFocus />
-              <div className="pointer-events-none absolute inset-y-0 right-4 flex items-center">
-                <Search className="size-5" />
-              </div>
-            </div>
-            <div onClick={handleCloseClick}>
-              <IconButton icon={<X id="close-button" className="size-5" />} />
-            </div>
-          </div>
-          {searchQuery.length >= 2 && (
-            <Card size="sm" className="absolute top-11 -mx-1 w-[89%]">
-              <div className="flex flex-col gap-1">
-                {isPending ? (
-                  <SearchNavSkeletons />
-                ) : filteredUsers.length > 0 ? (
-                  filteredUsers.map(user => (
-                    <Link onClick={handleLink} className="group flex items-center gap-1 py-1" to={`/profil/${user.id}`} key={user.id}>
-                      <Avatar size="sm" src={user.profilePhoto || '/uploads/profil.png'} alt={`Photo de ${user.pseudo}`} className="mr-2 size-8" />
-                      <span className="text-neutral-11 group-hover:text-green-11">{user.pseudo}</span>
-                    </Link>
-                  ))
-                ) : (
-                  <p className="p-2 text-sm text-neutral-11">Aucun résultat</p>
-                )}
-              </div>
-            </Card>
-          )}
-        </div>
-      )}
+      </div>
+      {isModalOpen && <SearchModal closeModal={closeModal} />}
     </>
   );
 }
